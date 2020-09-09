7e4e0664acc67e74eb14bd756aa180df3e9e9317
Sviatoslav Tsyfrak <sviatoslav1616@gmail.com>
2020-09-09 13:22:35 +0300

    [INIT] Init commit

---
+++ presentation/src/main/java/com/example/composeweatherapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.composeweatherapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.example.composeweatherapp.ui.ComposeWeatherAppTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeWeatherAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeWeatherAppTheme {
+        Greeting("Android")
+    }
+}
